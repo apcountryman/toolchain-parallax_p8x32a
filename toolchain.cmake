8967ebfd9ba734b0197ad790b7fa5bb6bb712c3b
Andrew Countryman <apcountryman@gmail.com>
2017-05-05 17:40:12 -0700

    Enhance parallax_p8x32a_add_load_target()
    Add example program load options for board configuration variable
    patching, board include path addition, port selection, loader delay, and
    loader verbosity.

--- toolchain.cmake
+++ toolchain.cmake
@@ -62,33 +62,71 @@ if ( ${VALID_MEMORY_MODEL} LESS 0 )
 
 endif ( ${VALID_MEMORY_MODEL} LESS 0 )
 
-# Add a basic load target for an executable.
+# Add a load target (<executable>-load) for an executable.
 #
 # SYNOPSIS:
 #       parallax_p8x32a_add_load_target( <executable>
-#                                        [EEPROM] [RUN] [TERMINAL]
-#                                        [BOARD <board>] )
+#                                        [BOARD <board>]
+#                                        [DEFINE] <variable> ...]
+#                                        [EEPROM]
+#                                        [INCLUDE <path> ...]
+#                                        [PORT <port>]
+#                                        [QUIT]
+#                                        [RUN]
+#                                        [SLOW <delay>]
+#                                        [TERMINAL <baud>]
+#                                        [VERBOSE] )
 #
 # OPTIONS:
 #       BOARD <board>
 #           Adds propeller-load's "-b <type>" option where "<type>" is <board>.
+#       DEFINE <variable> ...
+#           Adds propeller-load's "-D var=value" option where "var=value" is "<variable>".
 #       EEPROM
 #           Adds propeller-load's "-e" option.
 #       <executable>
 #           The name of the executable to make the load target for.
+#       INCLUDE <path> ...
+#           Adds propeller-load's "-I <path>" option.
+#       PORT <port>
+#           Adds propeller-load's "-p <port>" option.
+#       QUIT
+#           Adds propeller-load's "-q" option.
 #       RUN
 #           Adds propeller-load's "-r" option.
-#       TERMINAL
-#           Adds propeller-load's "-t" option.
+#       SLOW <delay>
+#           Adds propeller-load's "-S<n>" option where "<n>" is "<delay>". If "<delay>" is
+#           "DEFAULT", the default delay will be used.
+#       TERMINAL <baud>
+#           Adds propeller-load's "-t<baud>" option. If "<baud>" is "DEFAULT", the default
+#           baud rate will be used.
+#       VERBOSE
+#           Adds propeller-load's "-v" option.
 # EXAMPLES:
 #       parallax_p8x32a_add_load_target( foo RUN )
 #       parallax_p8x32a_add_load_target( foo RUN BOARD eeprom )
-#       parallax_p8x32a_add_load_target( foo RUN TERMINAL BOARD eeprom )
+#       parallax_p8x32a_add_load_target( foo RUN TERMINAL 115200 BOARD eeprom )
 #       parallax_p8x32a_add_load_target( foo EEPROM RUN BOARD eeprom )
 function( parallax_p8x32a_add_load_target EXECUTABLE )
-    set( options EEPROM RUN TERMINAL )
-    set( one_value_args BOARD )
-    set( multi_value_args )
+    set(
+        options
+        EEPROM
+        QUIT
+        RUN
+        VERBOSE
+    )
+    set(
+        one_value_args
+        BOARD
+        PORT
+        SLOW
+        TERMINAL
+    )
+    set(
+        multi_value_args
+        DEFINE
+        INCLUDE
+    )
     include( CMakeParseArguments )
     cmake_parse_arguments(
         parallax_p8x32a_add_load_target
@@ -98,33 +136,90 @@ function( parallax_p8x32a_add_load_target EXECUTABLE )
         ${ARGN}
     )
 
+    # ensure there are no unrecognized arguments
+    if( ${parallax_p8x32a_add_load_target_UNPARSED_ARGUMENTS} )
+        message(
+            FATAL_ERROR
+            "'${parallax_p8x32a_add_load_target_UNPARSED_ARGUMENTS}' are not recognized arguments"
+        )
+    endif( ${parallax_p8x32a_add_load_target_UNPARSED_ARGUMENTS} )
+
     # configure loader arguments
-    set( loader_args "" )
+    set( loader_flags "" )
 
-    if( parallax_p8x32a_add_load_target_EEPROM )
-        set( loader_args ${loader_args} "-e" )
+    if( ${parallax_p8x32a_add_load_target_EEPROM} )
+        list( APPEND loader_flags "-e" )
 
-    endif( parallax_p8x32a_add_load_target_EEPROM )
+    endif( ${parallax_p8x32a_add_load_target_EEPROM} )
 
-    if( parallax_p8x32a_add_load_target_RUN )
-        set( loader_args ${loader_args} "-r" )
+    if( ${parallax_p8x32a_add_load_target_QUIT} )
+        list( APPEND loader_flags "-q" )
 
-    endif( parallax_p8x32a_add_load_target_RUN )
+    endif( ${parallax_p8x32a_add_load_target_QUIT} )
 
-    if( parallax_p8x32a_add_load_target_TERMINAL )
-        set( loader_args ${loader_args} "-t" )
+    if( ${parallax_p8x32a_add_load_target_RUN} )
+        list( APPEND loader_flags "-r" )
 
-    endif( parallax_p8x32a_add_load_target_TERMINAL )
+    endif( ${parallax_p8x32a_add_load_target_RUN} )
+
+    if( ${parallax_p8x32a_add_load_target_VERBOSE} )
+        list( APPEND loader_flags "-v" )
+
+    endif( ${parallax_p8x32a_add_load_target_VERBOSE} )
 
     if( NOT "${parallax_p8x32a_add_load_target_BOARD}" STREQUAL "" )
-        set( loader_args ${loader_args} "-b" ${parallax_p8x32a_add_load_target_BOARD} )
+        list( APPEND loader_flags "-b" ${parallax_p8x32a_add_load_target_BOARD} )
 
     endif( NOT "${parallax_p8x32a_add_load_target_BOARD}" STREQUAL "" )
+
+    if( NOT "${parallax_p8x32a_add_load_target_PORT}" STREQUAL "" )
+        list( APPEND loader_flags "-p" ${parallax_p8x32a_add_load_target_PORT} )
+
+    endif( NOT "${parallax_p8x32a_add_load_target_PORT}" STREQUAL "" )
+
+    if( NOT "${parallax_p8x32a_add_load_target_SLOW}" STREQUAL "" )
+        if( "${parallax_p8x32a_add_load_target_SLOW}" STREQUAL "DEFAULT" )
+            list( APPEND loader_flags "-S" )
+
+        else( "${parallax_p8x32a_add_load_target_SLOW}" STREQUAL "DEFAULT" )
+            list( APPEND loader_flags "-S${parallax_p8x32a_add_load_target_SLOW}" )
+
+        endif( "${parallax_p8x32a_add_load_target_SLOW}" STREQUAL "DEFAULT" )
+
+    endif( NOT "${parallax_p8x32a_add_load_target_SLOW}" STREQUAL "" )
+
+    if( NOT "${parallax_p8x32a_add_load_target_TERMINAL}" STREQUAL "" )
+        if( "${parallax_p8x32a_add_load_target_TERMINAL}" STREQUAL "DEFAULT" )
+            list( APPEND loader_flags "-t" )
+
+        else( "${parallax_p8x32a_add_load_target_TERMINAL}" STREQUAL "DEFAULT" )
+            list( APPEND loader_flags "-t${parallax_p8x32a_add_load_target_TERMINAL}" )
+
+        endif( "${parallax_p8x32a_add_load_target_TERMINAL}" STREQUAL "DEFAULT" )
+
+    endif( NOT "${parallax_p8x32a_add_load_target_TERMINAL}" STREQUAL "" )
+
+    if( NOT "${parallax_p8x32a_add_load_target_DEFINE}" STREQUAL "" )
+        foreach( config_variable IN ITEMS ${parallax_p8x32a_add_load_target_DEFINE} )
+            list( APPEND loader_flags "-D" ${config_variable} )
+
+        endforeach( config_variable IN ITEMS ${parallax_p8x32a_add_load_target_DEFINE} )
+
+    endif( NOT "${parallax_p8x32a_add_load_target_DEFINE}" STREQUAL "" )
+
+    if( NOT "${parallax_p8x32a_add_load_target_INCLUDE}" STREQUAL "" )
+        foreach( path IN ITEMS ${parallax_p8x32a_add_load_target_INCLUDE} )
+            list( APPEND loader_flags "-I" ${path} )
+
+        endforeach( path IN ITEMS ${parallax_p8x32a_add_load_target_INCLUDE} )
+
+    endif( NOT "${parallax_p8x32a_add_load_target_INCLUDE}" STREQUAL "" )
 
     # add the load target
     add_custom_target(
         ${EXECUTABLE}-load
-        COMMAND ${PARALLAX_P8X32A_LOADER} ${loader_args} ${EXECUTABLE}
+        COMMAND ${PARALLAX_P8X32A_LOADER} ${loader_flags} ${EXECUTABLE}
         DEPENDS ${EXECUTABLE}
     )
+
 endfunction( parallax_p8x32a_add_load_target )
